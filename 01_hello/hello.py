6d88c088432a2b82462eed88793162c285a6b0c1
Tyson <zibrah3ed@gmail.com>
2021-06-23 19:04:39 -0500

    Chapter onwe 6/23

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -1,3 +1,30 @@
 #! /Users/tyson/.virtualenvs/tiny_python_projects/bin/python
-# Purpose: Say Hello
-print('Hello, World!')
+"""
+Authors: TEF, KYC
+Purpose: Say Hello
+"""
+
+
+import argparse
+
+
+def get_args():
+    '''
+    Add parameter and help to program
+    '''
+    parser = argparse.ArgumentParser(description='Say Hello')
+    parser.add_argument('-n', '--name', metavar='name',
+                        default='World', help='Name to greet')
+    return parser.parse_args()
+
+
+def main():
+    '''
+    Main function with Jazz hands
+    '''
+    args = get_args()
+    print('Hello, ' + args.name + '!')
+
+
+if __name__ == '__main__':
+    main()
